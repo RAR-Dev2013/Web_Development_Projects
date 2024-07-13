1f86ceb9dde8e93f69fd8464d75a61622110dd80
Roxanne Reindl <rreindl13@gmail.com>
2024-07-12 18:24:35 -0700

    added edit feature

--- ToDoList/Todo.js
+++ ToDoList/Todo.js
@@ -1,21 +1,20 @@
 //TO DO LIST//
 //Clean up prompt message
 
-
-
 let counter = 0;
 
-let toDoList = ["new"];
+let toDoList = [];
 
 //While loop
 while(true) {
     //First prompt
-    let msg = prompt("Welcome to your todo list, instructions below:\n \"new\" adds to arraylist.\n \"list\" shows all to do's in list.\n \"delete\" will delete a specific to do.\n \"quit\" will end the program");
+    let msg = prompt("Welcome to your todo list, instructions below:\n \"new\" adds to arraylist.\n \"list\" shows all to do's in list.\n \"edit\" will edit a specific item.\n \"delete\" will delete a specific to do.\n \"quit\" will end the program");
 
     if (msg.toLowerCase() === "new") {
         //Adds new item to the ToDoList
         let newToDo = prompt("Add your To Do list item.")
         toDoList.push(newToDo); 
+        console.log("The list includes" + toDoList)
 
     } else if (msg.toLowerCase() === "list") {
         //Shows all to do's in list
@@ -23,6 +22,24 @@ while(true) {
             console.log(toDo);
             continue;
         }
+
+    } else if (msg.toLowerCase() === "edit") {
+        //Shows all to do's in list
+        for (let toDo of toDoList) {
+            let items = JSON.stringify(toDo);
+        }
+
+        let editToDo = prompt(`Which to-do item do you want to edit?`);
+        let replaceToDo = prompt("What item would you like instead");
+
+        //find indexof
+        let editedItem = toDoList.indexOf(editToDo);
+        console.log(editedItem)
+
+        //add indexof to splice
+        const removed = toDoList.splice(editedItem, 1, replaceToDo);
+
+        console.log(toDoList);
 
     } else if (msg.toLowerCase() === "delete") {
         //removes specific todo from arraybased on text
